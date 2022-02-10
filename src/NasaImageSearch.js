e25319d92bfac0664090d867e9fb55580a73ab22
mayormaier <maierandrewd@gmail.com>
2022-02-10 12:18:06 -0500

    starting point

--- src/NasaImageSearch.js
+++ src/NasaImageSearch.js
@@ -1,49 +1,98 @@
+/* eslint-disable lit-a11y/no-autofocus */
 import { html, css, LitElement } from 'lit';
 import '@lrnwebcomponents/accent-card';
 
 export class NasaImageSearch extends LitElement {
-  static get tag() {
-    return 'nasa-image-search';
-  }
-
   static get styles() {
     return css`
       :host {
         display: block;
         padding: 25px;
         color: var(--nasa-image-search-text-color, #000);
+        font-family: 'sans-serif';
+        font-family: 'Open Sans', sans-serif;
       }
 
-      :host([view='list']) ul {
-        margin: 20px;
+      img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+
+      h2 {
+        display: block;
+        font-size: 3em;
+        margin-top: 0.83em;
+        margin-bottom: 0.83em;
+        margin-left: 0;
+        margin-right: 0;
+        font-weight: bold;
+      }
+
+      .button1 {
+        border: none;
+        background-color: #ff7376;
+        color: black;
+        padding: 10px 22px;
+        text-align: center;
+        display: inline-block;
+        font-size: 14px;
+        margin: 2px 2px;
+        cursor: pointer;
+      }
+
+      .button2 {
+        border: none;
+        background-color: #ff7376;
+        color: black;
+        padding: 10px 22px;
+        text-align: center;
+        display: inline-block;
+        font-size: 14px;
+        margin: 2px 2px;
+        cursor: pointer;
+      }
+
+      input[type='text'] {
+        width: 30%;
+        padding: 8px 20px;
+        margin: 8px 0;
+        display: inline-block;
+      }
+
+      .center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 40px;
       }
     `;
   }
 
   static get properties() {
     return {
-      nasaResults: { type: Array },
-      loadData: { type: Boolean, reflect: true, attribute: 'load' },
-      view: { type: String, reflect: true },
+      searchTerm: { type: String, reflect: true },
+      images: { type: Array },
     };
   }
 
-  firstUpdated(changedProperties) {
-    if (super.firstUpdated) {
-      super.firstUpdated(changedProperties);
-    }
-    this.getNASAData();
+  constructor() {
+    super();
+    this.images = [];
+    this.searchTerm = '';
   }
 
   updated(changedProperties) {
     changedProperties.forEach((oldValue, propName) => {
-      if (propName === 'loadData' && this[propName]) {
-        this.getNASAData();
-      } else if (propName === 'nasaResults') {
+      if (propName === 'searchTerm' && this[propName]) {
+        this.getData();
+      } else if (propName === 'images') {
+        this.render();
+      } else if (propName === 'images') {
         this.dispatchEvent(
           new CustomEvent('results-changed', {
             detail: {
-              value: this.nasaResults,
+              value: this.images,
             },
           })
         );
@@ -51,76 +100,55 @@ export class NasaImageSearch extends LitElement {
     });
   }
 
-  async getNASAData() {
-    return fetch(
-      'https://images-api.nasa.gov/search?q=moon&page=1&media_type=image'
-    )
-      .then(resp => {
-        if (resp.ok) {
-          return resp.json();
-        }
-        return false;
-      })
+  getData() {
+    const apiURL = 'https://images-api.nasa.gov/search?media_type=image&q=';
+    fetch(apiURL + this.searchTerm)
+      .then(response => response.json())
       .then(data => {
-        console.log(data);
-        this.nasaResults = [];
-
-        data.collection.items.forEach(element => {
-          if (element.links[0].href !== undefined) {
-            const moonInfo = {
-              imagesrc: element.links[0].href,
-              title: element.data[0].title,
-              description: element.data[0].description,
-            };
-            console.log(moonInfo);
-            this.nasaResults.push(moonInfo);
-          }
-        });
-        return data;
+        this.images = [];
+        const imageCollection = new Array(data.collection.items);
+        for (let i = 0; i < imageCollection[0].length; i += 1) {
+          this.images.push(imageCollection[0][i]);
+        }
       });
   }
 
-  resetData() {
-    this.nasaResults = [];
-    this.loadData = false;
+  updateSearchTerm() {
+    this.searchTerm = this.shadowRoot.querySelector('#searchTerm').value;
   }
 
-  constructor() {
-    super();
-    this.nasaResults = [];
-    this.loadData = false;
-    this.view = 'accent-card';
+  clearFields() {
+    this.shadowRoot.querySelector('#searchTerm').value = '';
+    this.images = [];
   }
-  // this is purely so my vscode will push to github
 
   render() {
+    const detailsURL = 'https://images.nasa.gov/details-';
     return html`
-      ${this.view === `list`
-        ? html`
-            <ul>
-              ${this.nasaResults.map(
-                item => html`
-                  <li>
-                    ${item.imagesrc} - ${item.title} - ${item.description}
-                  </li>
-                `
-              )}
-            </ul>
-          `
-        : html`
-            ${this.nasaResults.map(
-              item => html`
-                <accent-card
-                  image-src="${item.imagesrc}"
-                  image-align="right"
-                  horizontal
-                >
-                  <div slot="heading">${item.title}</div>
-                  <div slot="content">${item.description}</div>
-                </accent-card>
-              `
-            )}
-          `}
+      <img src="../assets/favicon-192.png" alt="nasa logo" style="width:128px;height:128px;">
+      <h2 style="text-align:center">NASA Search!</h2>
+
+      <div class="center">
+        <button class="button1" @click=${this.clearFields}>Reset</button>
+        <input type="text" id="searchTerm" autofocus></input>
+        <button class="button2" @click=${this.updateSearchTerm}>Search!</button>
+      </div>
+      <br><br>
+      ${this.images.map(
+        item => html`
+          <a href="${detailsURL}${item.data[0].nasa_id}" target="_blank">
+            <accent-card
+              image-src=${item.links[0].href}
+              accent-color="red"
+              horizontal
+              style="max-width:300%;"
+            >
+              <div slot="heading">${item.data[0].title}</div>
+              <div slot="content">${item.data[0].description}</div>
+            </accent-card>
+          </a>
+        `
+      )}
     `;
   }
 }
